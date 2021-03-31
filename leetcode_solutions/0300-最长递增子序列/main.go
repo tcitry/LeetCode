2082574a6a8c4479168dbe16427cc322cacb3c3c
tcitry <tcitry@gmail.com>
2021-03-31 16:19:35 +0800

    add solutions

--- leetcode_solutions/0300-最长递增子序列/main.go
+++ leetcode_solutions/0300-最长递增子序列/main.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println(lengthOfLIS([]int{1,2,3}))
+func main() {
+	fmt.Println(lengthOfLIS([]int{1, 2, 3}))
 }
 
 func lengthOfLIS(nums []int) int {
